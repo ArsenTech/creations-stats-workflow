8b6ca5a7bdf5fa887e21901f53b31f5da26a89fc
ArsenTech <62609185+ArsenTech@users.noreply.github.com>
2025-12-29 20:40:03 +0400

    Installed Rollup and Compiled the action

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+try {
+    const testInput = core.getInput("test-in");
+    core.info(testInput);
+    core.setOutput("test-out", "Hello world");
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    core.info(`Paylod: ${payload}`);
+}
+catch (error) {
+    core.setFailed(error.message);
+}
+//# sourceMappingURL=index.js.map
---
+++ rollup.config.ts
@@ -0,0 +1,24 @@
+import typescript from "@rollup/plugin-typescript"
+import nodeResolve from "@rollup/plugin-node-resolve"
+
+const config = {
+     input: "src/index.ts",
+     output: {
+          esModule: true,
+          file: "dist/index.js",
+          format: "esm",
+          sourcemap: true
+     },
+     plugins: [
+          typescript({
+               tsconfig: "./tsconfig.json"
+          }),
+          nodeResolve({
+               preferBuiltins: true,
+               modulesOnly: true
+          })
+     ],
+     external: ["@actions/core","@actions/github"]
+}
+
+export default config
