4203e70b1aa202596c33a3818fcf276f70e9c406
ArsenTech <62609185+ArsenTech@users.noreply.github.com>
2025-12-30 13:27:59 +0400

    made another test code + changed workflow options

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,26 @@ import * as core from "@actions/core"
 import * as github from "@actions/github"
 
 try{
-     const testInput = core.getInput("test-in");
-     core.info(testInput);
-
-     core.setOutput("test-out","Hello world");
-     const payload = JSON.stringify(github.context.payload,undefined,2);
-     core.info(`Paylod: ${payload}`)
+     const username = core.getInput("github-username");
+     const exclusions = core.getInput("exclusions");
+     const targetFile = core.getInput("target-file");
+     const repoLimit = core.getInput("repo-limit");
+     const gistLimit = core.getInput("gist-limit");
+     const showArchives = core.getBooleanInput("show-archives");
+     const showForks = core.getBooleanInput("show-forks");
+     const showGistStargazers = core.getInput("show-gist-stargazers");
+     const commitMessage = core.getInput("commit-message");
+     core.info(JSON.stringify({
+          username,
+          exclusions,
+          targetFile,
+          repoLimit,
+          gistLimit,
+          showArchives,
+          showForks,
+          showGistStargazers,
+          commitMessage
+     }))
 } catch (error: any){
-     core.setFailed(error.message)
+     core.setFailed(`[ERROR]: ${error.message}`)
 }
